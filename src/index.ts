1068fc641c7b32c5852875dff327f2d864358b9f
Erickson Teixeira <erickson@Ericksons-MacBook-Pro.local>
2023-10-26 13:19:18 -0300

    routing baby

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app from "./config/app";
+import setUpRoute from './config/routes'
 
 const port = 5000;
-app.listen(port, () => console.log(`App listening on http://localhost:${port}`))
 
-app.get('/', (req,res) => res.json('test'))
+setUpRoute(app)
+
+app.listen(port, () => console.log(`App listening on http://localhost:${port}`))
